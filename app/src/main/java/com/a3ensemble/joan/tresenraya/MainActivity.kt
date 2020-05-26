3d7dc0859ae6d4f6ad48d4734df74ce715c59f3e
joan-tg <joantormogarcia@gmail.com>
2020-05-27 00:17:44 +0200

    Botón Salir y quitada ActionBar

--- app/src/main/java/com/a3ensemble/joan/tresenraya/MainActivity.kt
+++ app/src/main/java/com/a3ensemble/joan/tresenraya/MainActivity.kt
@@ -2,11 +2,17 @@ package com.a3ensemble.joan.tresenraya
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+        boton_salir.setOnClickListener { finish() }
     }
 }
